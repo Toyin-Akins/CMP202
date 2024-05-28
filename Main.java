f6c6696264093b2eaad70fc5c73c86f868549c3f
Toyin-Akins <130286458+Toyin-Akins@users.noreply.github.com>
2024-05-28 15:50:01 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class main{
+    public static void main(){
+        String word = "Hannah";
+        char[] wordList = word.toCharArray();
+        for (i = wordList.length -1, i >= 0, i--){
+            String word2 += wordList[i]; 
+        }
+        if (word == word2){
+            System.out.println("The word is the same backword");
+        }
+        else{
+            System.out.println("The word is not the same backword");
         }
     }
 }
